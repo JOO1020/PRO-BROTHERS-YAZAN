d76e29d21cc51a6763c15c1b385269a30a900a3f
JOO1020 <ym6690897@gmail.com>
2023-10-22 23:57:45 +0300

    end

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,42 @@
 
+// const submitBtn = document.getElementById('button');
+
+// document.getElementById('form')
+// .addEventListener('submit', function(event) {
+// event.preventDefault();
+
+// submitBtn.value = 'Sending...';
+
+// const serviceID = 'default_service';
+// const templateID = 'template_itpv483';
+
+// emailjs.sendForm(serviceID, templateID, this)
+//     .then(() => {
+//     submitBtn.value = 'Send Email';
+//     alert('Sent!');
+//     }, (err) => {
+//     submitBtn.value = 'Send Email';
+//     alert(JSON.stringify(err));
+//     });
+// });
+//
 const submitBtn = document.getElementById('button');
 
 document.getElementById('form')
 .addEventListener('submit', function(event) {
 event.preventDefault();
 
-submitBtn.value = 'Sending...';
+    btn.value = 'Sending...';
 
 const serviceID = 'default_service';
-const templateID = 'template_itpv483';
+const templateID = 'template_ruup8hn';
 
 emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-    submitBtn.value = 'Send Email';
+    btn.value = 'Send Email';
     alert('Sent!');
     }, (err) => {
-    submitBtn.value = 'Send Email';
+    btn.value = 'Send Email';
     alert(JSON.stringify(err));
     });
 });
