052e5a67f01bf858d46e7ff8af6e186b6b3a528a
JOO1020 <ym6690897@gmail.com>
2023-12-31 00:18:46 +0200

    تم

--- js/contact.js
+++ js/contact.js
@@ -1,27 +1,5 @@
 
-// const submitBtn = document.getElementById('button');
-
-// document.getElementById('form')
-// .addEventListener('submit', function(event) {
-// event.preventDefault();
-
-// submitBtn.value = 'Sending...';
-
-// const serviceID = 'default_service';
-// const templateID = 'template_itpv483';
-
-// emailjs.sendForm(serviceID, templateID, this)
-//     .then(() => {
-//     submitBtn.value = 'Send Email';
-//     alert('Sent!');
-//     }, (err) => {
-//     submitBtn.value = 'Send Email';
-//     alert(JSON.stringify(err));
-//     });
-// });
-//
 const submitBtn = document.getElementById('button');
-
 document.getElementById('form')
 .addEventListener('submit', function(event) {
 event.preventDefault();
